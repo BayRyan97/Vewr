4062718ea59a4d7d29c00b2cb36a89a5f39818cc
BayRyan97 <135568984+BayRyan97@users.noreply.github.com>
2023-06-05 21:54:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const themeSwitch = document.querySelector('#theme-switch');
 
 themeSwitch.addEventListener('change', () => {
-    toggleNightMode(document.body);
-    toggleNightMode(document.querySelector('header'));
-    toggleNightMode(document.querySelector('.hero'));
-    toggleNightMode(document.querySelector('.footer'));
+    toggleDayMode(document.body);
+    toggleDayMode(document.querySelector('header'));
+    toggleDayMode(document.querySelector('.hero'));
+    toggleDayMode(document.querySelector('.footer'));
 
     let benefitCards = document.querySelectorAll('.benefit-card');
     benefitCards.forEach((card) => {
-        toggleNightMode(card);
+        toggleDayMode(card);
     });
 });
 
-function toggleNightMode(element) {
-    element.classList.toggle('night');
+function toggleDayMode(element) {
+    element.classList.toggle('day');
 }
